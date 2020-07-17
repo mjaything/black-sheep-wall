d4dd3e0f14574c4b6dae8c67838b4b9e44209d77
Minjun Kim <mjaything@gmail.com>
2020-07-18 01:04:35 +0200

    fix: solve inconsistency of res, response

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
 import express from "express";
 
 const userRouter = express.Router();
-const handleUserIndex = (req, response) => res.send("user index");
-const handleUserEdit = (req, response) => res.send("user edit");
+const handleUserIndex = (req, res) => res.send("user index");
+const handleUserEdit = (req, res) => res.send("user edit");
 const handleUserPassword = (req, res) => res.send("user password");
 
 userRouter.get("/", handleUserIndex);
