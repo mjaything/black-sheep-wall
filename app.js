5239baa53a8c3228d304f5e7863de54944007789
Minjun Kim <mjaything@gmail.com>
2020-07-18 00:27:31 +0200

    fix: remove unnecessary test components

--- app.js
+++ app.js
@@ -4,14 +4,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 const app = express();
-const PORT = 4000;
 
 const handleProfile = (request, response) => console.log("You're at Profile");
 const handleHome = (request, response) => response.send("Hello");
-const handleListening = () =>
-  console.log(`Listening to: https://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
 
 // middleware logger
 app.use(morgan("dev"));
