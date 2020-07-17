e472913871f0dd0495fbbb30fc725435c00d48b1
Minjun Kim <mjaything@gmail.com>
2020-07-18 01:09:45 +0200

    feat: allow to export userRouter

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 import express from "express";
 
-const userRouter = express.Router();
+export const userRouter = express.Router();
+
 const handleUserIndex = (req, res) => res.send("user index");
 const handleUserEdit = (req, res) => res.send("user edit");
 const handleUserPassword = (req, res) => res.send("user password");
