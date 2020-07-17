9837906fc120d7902a7561a7d149242d6a8b08d2
Minjun Kim <mjaything@gmail.com>
2020-07-17 21:54:41 +0200

    feat: add middleware morgan

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 const app = express();
 const PORT = 4000;
 
@@ -8,6 +9,9 @@ const handleListening = () =>
   console.log(`Listening to: https://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
+
+// middleware logger
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
