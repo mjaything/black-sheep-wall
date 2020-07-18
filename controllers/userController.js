ee7d73353e39f3d662e129a1f32daa295a05af39
Minjun Kim <mjaything@gmail.com>
2020-07-19 00:45:49 +0200

    feat: replace send with render in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const user = (req, res) => res.send("User");
-export const userInfo = (req, res) => res.send("User Info");
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
-export const editProfile = (req, res) => res.send("Edit Profile");
-export const changePassword = (req, res) => res.send("Change Password");
+export const user = (req, res) => res.render("User");
+export const userInfo = (req, res) => res.render("User Info");
+export const join = (req, res) => res.render("Join");
+export const login = (req, res) => res.render("Login");
+export const logout = (req, res) => res.render("Logout");
+export const editProfile = (req, res) => res.render("Edit Profile");
+export const changePassword = (req, res) => res.render("Change Password");
