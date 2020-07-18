d0ef12467de258e36eb17349e08b1d149e4a8717
Minjun Kim <mjaything@gmail.com>
2020-07-18 20:51:25 +0200

    feat: add rest of the video controller constants

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,2 +1,7 @@
 export const home = (req, res) => res.send("Home");
 export const search = (req, res) => res.send("Search");
+export const video = (req, res) => res.send("Video");
+export const uploadVideo = (req, res) => res.send("Upload Video");
+export const videoInfo = (req, res) => res.send("Video Info");
+export const editVideo = (req, res) => res.send("Edit Video");
+export const deleteVideo = (req, res) => res.send("Delete Video");
