5b0cf6d1fbad34371eff88649e2b628c3c0539fe
Minjun Kim <mjaything@gmail.com>
2020-07-20 06:41:44 +0200

    feat: divide routers of sign up, log in into get, post

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,23 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { signUp, logIn, logOut } from "../controllers/userController";
+import {
+  getSignUp,
+  postSignUp,
+  getLogIn,
+  postLogIn,
+  logOut,
+} from "../controllers/userController";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.signUp, getSignUp);
+globalRouter.post(routes.signUp, postSignUp);
+globalRouter.get(routes.logIn, getLogIn);
+globalRouter.get(routes.logIn, postLogIn);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.signUp, signUp);
-globalRouter.get(routes.logIn, logIn);
 globalRouter.get(routes.logOut, logOut);
 
 export default globalRouter;
