1b7338104f85a48ad1dfc96c4e035f098c0f7b9e
Minjun Kim <mjaything@gmail.com>
2020-07-20 07:13:24 +0200

    feat: make userInfo execute

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -12,6 +12,6 @@ const userRouter = express.Router();
 userRouter.get(routes.user, user);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userInfo, userInfo);
+userRouter.get(routes.userInfo(), userInfo);
 
 export default userRouter;
