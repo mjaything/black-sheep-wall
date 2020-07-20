19a9794f3dca8357210fb1ba249109f18ebcb181
Minjun Kim <mjaything@gmail.com>
2020-07-20 08:27:37 +0200

    feat: redirect log out to home

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,13 +18,16 @@ export const postSignUp = (req, res) => {
 
 export const getLogIn = (req, res) =>
   res.render("logIn", { pageTitle: "Log In" });
+
 export const postLogIn = (req, res) => res.redirect(routes.home);
+
+export const logOut = (req, res) => {
+  res.redirect(routes.home);
+};
 
 export const user = (req, res) => res.render("user", { pageTitle: "User" });
 export const userInfo = (req, res) =>
   res.render("userInfo", { pageTitle: "User Info" });
-export const logOut = (req, res) =>
-  res.render("logOut", { pageTitle: "Log Out" });
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
