045e458a5dee5099084ac233326c7d215b1fc434
Minjun Kim <mjaything@gmail.com>
2020-07-20 03:03:02 +0200

    feat: add temporary database for test

---
+++ db.js
@@ -0,0 +1,41 @@
+export const videos = [
+  {
+    id: 9999,
+    title: "Awesome Video",
+    description: "This is an awesome video!",
+    views: 10,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 999,
+      name: "Minjun",
+      email: "minjun@minjun.com",
+    },
+  },
+  {
+    id: 99999,
+    title: "Awesome Video2",
+    description: "This is an awesome video!",
+    views: 10,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 999,
+      name: "Minjun",
+      email: "minjun@minjun.com",
+    },
+  },
+  {
+    id: 9999999,
+    title: "Awesome Video3",
+    description: "This is an awesome video!",
+    views: 10,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 999,
+      name: "Minjun",
+      email: "minjun@minjun.com",
+    },
+  },
+];
