d43024f95345ea4fb2cafce1e24872fd7494bd75
Minjun Kim <mjaything@gmail.com>
2020-07-20 05:13:34 +0200

    feat: add videos to render template in search constant in videoController

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,15 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+import { videos } from "../db";
+
+export const home = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos });
 export const search = (req, res) => {
   const {
     query: { keyword: searchKeyword },
   } = req;
-  res.render("search", { pageTitle: "Search", searchKeyword });
+  res.render("search", { pageTitle: "Search", searchKeyword, videos });
 };
-export const video = (req, res) => res.render("video", { pageTitle: "Video" });
+export const video = (req, res) =>
+  res.render("video", { pageTitle: "Video", videos });
 export const uploadVideo = (req, res) =>
   res.render("uploadVideo", { pageTitle: "Upload Video" });
 export const videoInfo = (req, res) =>
