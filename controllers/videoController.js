0e1db58dfd5c5d09e33c0c8fd5470c7295aa8346
Minjun Kim <mjaything@gmail.com>
2020-07-20 08:28:13 +0200

    feat: divide uploadVideo into get, post

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) =>
   res.render("home", { pageTitle: "Home", videos });
@@ -8,10 +9,19 @@ export const search = (req, res) => {
   } = req;
   res.render("search", { pageTitle: "Search", searchKeyword, videos });
 };
+
+export const getUploadVideo = (req, res) =>
+  res.render("uploadVideo", { pageTitle: "Upload Video" });
+
+export const postUploadVideo = (req, res) => {
+  const {
+    body: { videoFile, videoTitle, videoDescription },
+  } = req;
+  res.redirect(routes.videoInfo(9999));
+};
+
 export const video = (req, res) =>
   res.render("video", { pageTitle: "Video", videos });
-export const uploadVideo = (req, res) =>
-  res.render("uploadVideo", { pageTitle: "Upload Video" });
 export const videoInfo = (req, res) =>
   res.render("videoInfo", { pageTitle: "Video Info" });
 export const editVideo = (req, res) =>
