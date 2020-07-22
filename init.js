58cb824da5bed3794adc63ae983605d0d62dd005
Minjun Kim <mjaything@gmail.com>
2020-07-22 03:50:29 +0200

    feat: assign a value for PORT constant

--- init.js
+++ init.js
@@ -1,7 +1,10 @@
 import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4000;
+// assign 4000 in case PORT constant from .env is not found
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`${PORT}`);
 
